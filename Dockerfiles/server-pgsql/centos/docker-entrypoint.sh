f30fdce151581f1825eb0fd2c64f6618e1a1435f
Alexey Pustovalov <alexey.pustovalov@zabbix.com>
2024-12-25 14:41:51 +0900

    Fixed duplicate Include parameters for agent and agent2 images

--- Dockerfiles/server-pgsql/centos/docker-entrypoint.sh
+++ Dockerfiles/server-pgsql/centos/docker-entrypoint.sh
@@ -106,7 +106,9 @@ update_config_var() {
     var_value=$(escape_spec_char "$var_value")
     var_name=$(escape_spec_char "$var_name")
 
-    if [ "$(grep -E "^$var_name=" $config_path)" ] && [ "$is_multiple" != "true" ]; then
+    if [ "$(grep -E "^$var_name=$var_value" $config_path)" ]; then
+        echo "exists"
+    elif [ "$(grep -E "^$var_name=" $config_path)" ] && [ "$is_multiple" != "true" ]; then
         sed -i -e "/^$var_name=/s/=.*/=$var_value/" "$config_path"
         echo "updated"
     elif [ "$(grep -Ec "^# $var_name=" $config_path)" -gt 1 ]; then
